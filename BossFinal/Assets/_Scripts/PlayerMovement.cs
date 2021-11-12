2b85e633efa7cf9611ab0edb62bee2283bb2706f
JoseFernandoInsper <josefmc@al.insper.edu.br>
2021-11-12 17:16:54 -0300

    basic caracter controller

---
+++ BossFinal/Assets/_Scripts/PlayerMovement.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    float horizontalMove = 0f;
+    public float Speed = 35f;
+    bool jump = false;
+    private float isFacing = 1;
+
+    void Update()
+    {
+
+        if(Math.Abs(Input.GetAxisRaw("Horizontal"))>= 0.5f)
+        {
+            isFacing = Input.GetAxisRaw("Horizontal");
+        }
+        
+        if (!controller.m_Grounded)
+        {
+            horizontalMove = isFacing * Speed;
+        }
+        if (controller.m_Grounded)
+        {
+            horizontalMove = 0f;
+        }
+        if (Input.GetButtonDown("Jump"))
+        {
+            
+            jump = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+}
