e67c70e7225729c78a4c3a15adcbfbbab7a17c55
JoseFernandoInsper <josefmc@al.insper.edu.br>
2021-11-19 16:41:28 -0300

    timerHUD

---
+++ BossFinal/Assets/_Scripts/TimerController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using TMPro;
+
+public class TimerController : MonoBehaviour
+{
+    TextMeshProUGUI timeCouter;
+    private float elapsedTime;
+    private TimeSpan timePlaying;
+
+    void Start()
+    {
+        timeCouter = GetComponent<TextMeshProUGUI>();
+        elapsedTime = 0f;
+    }
+
+    void Update()
+    {
+        timePlaying = TimeSpan.FromSeconds(elapsedTime);
+        string temp = timePlaying.ToString("mm':'ss'.'ff");
+        timeCouter.text = $"Tempo: {temp}";
+    }
+}
